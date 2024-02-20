9a0c58cef22da5fde21cab2a4b48aff9d831aafe
albertobatalha1970 <pcformate.albertobatalha@gmail.com>
2024-02-20 20:53:07 -0300

    Printando o preço de um ítem

--- response.py
+++ response.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 url = "https://raw.githubusercontent.com/albertobatalha1970/apialberto/main/eletronicos.json"
-response = requests.get(url)
-response = response.json()
-print(response)
+responses = requests.get(url)
+responses = responses.json()
+print("O preço do Tablet é: ")
+print(responses['Tablet DL']['Preço'])
