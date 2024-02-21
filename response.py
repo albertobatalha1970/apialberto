4d95bb7b8b33da018fa794849557f920ae3d0e76
albertobatalha1970 <pcformate.albertobatalha@gmail.com>
2024-02-20 22:11:42 -0300

    Novas strings adicionadas

--- response.py
+++ response.py
@@ -4,5 +4,5 @@ import json
 url = "https://raw.githubusercontent.com/albertobatalha1970/apialberto/main/api.json"
 responses = requests.get(url)
 responses = responses.json()
-print("O preço do Tablet é: ")
-print(responses['Aluno']['nome'])
+aluno = "O aluno tem "
+print(f'{aluno}', responses["Aluno"]["idade"], ' anos de idade!')
