0463bfa41817116a06243a4147ec6c4d0cd4c6ff
albertobatalha1970 <pcformate.albertobatalha@gmail.com>
2024-02-20 21:06:23 -0300

    Novas mudanças

--- response.py
+++ response.py
@@ -1,8 +1,8 @@
 import requests
 import json
 
-url = "https://raw.githubusercontent.com/albertobatalha1970/apialberto/main/eletronicos.json"
+url = "https://raw.githubusercontent.com/albertobatalha1970/apialberto/main/api.json"
 responses = requests.get(url)
 responses = responses.json()
 print("O preço do Tablet é: ")
-print(responses['Smartphone']['Preço'])
+print(responses['Aluno']['nome'])
